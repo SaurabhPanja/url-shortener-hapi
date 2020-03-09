c343d7621f1e0c128607c300e11efa21a73cbdea
Saurabh Panja <saurabh@market-pulse.in>
2020-03-09 15:04:15 +0530

    set up mongolab cloud mongodb

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const dotenv = require('dotenv');
 dotenv.config()
 
 //database connectivity
-
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/urlShortner', { 
+const mongodbURL = `mongodb://${process.env.DBUSER}:${process.env.DBPASSWORD}@ds041367.mlab.com:41367/url-shortner`;
+mongoose.connect(mongodbURL, { 
     useNewUrlParser: true, 
     useUnifiedTopology: true 
 }).then(() => console.log("Connected"));
